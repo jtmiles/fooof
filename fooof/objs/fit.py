6c74311e944cb76d7a625a1ad8f6c2666bfd37ed
Jesse Miles <jessemile@gmail.com>
2024-08-29 17:34:43 -0700

    Revert "Update fit.py"
    This reverts commit 13eb8b08af729a722ce983c85dc76179fcb7b3ba.

--- fooof/objs/fit.py
+++ fooof/objs/fit.py
@@ -534,7 +534,10 @@ class FOOOF():
 
             # Flatten the power spectrum using fit aperiodic fit
             self._spectrum_flat = self.power_spectrum - self._ap_fit
-                        
+            #### JTM edit - Aug 26, 2024 (see lines ~910 - 920 for motivation)
+            self._spectrum_flat[self._spectrum_flat < 0] = 0
+            
+            
             # Find peaks, and fit them with gaussians
             self.gaussian_params_ = self._fit_peaks(np.copy(self._spectrum_flat))
 
@@ -912,8 +915,8 @@ class FOOOF():
         #### https://doi.org/10.1038/s41467-024-50204-4
         #### End up with many spectra that dip below aperiodic fits
         #### causes systematic, age-related, frequency-specific RMSE estimates
-        # if min(flatspec) < 0:
-        #     flatspec -= min(flatspec)
+        if min(flatspec) < 0:
+            flatspec -= min(flatspec)
 
         # Use percentile threshold, in terms of # of points, to extract and re-fit
         perc_thresh = np.percentile(flatspec, self._ap_percentile_thresh)
